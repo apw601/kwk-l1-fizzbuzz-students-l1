172ca9cb4cf1f45c08ff88d8c5dfc07163cf8678
apw601 <apw601@gmail.com>
2018-06-05 21:51:16 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,14 @@
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    "Fizzbuzz"
+    elsif number % 5 == 0
+    "Buzz"
+    elsif number % 3 == 0
+    "Fizz"
+  end
+end
+
+puts fizzbuzz(15)
+puts fizzbuzz(10)
+puts fizzbuzz(9)
+puts fizzbuzz(4)
